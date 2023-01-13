c5e3428e9e98a4465bd894f71992f3c0060a6fb4
Santiago Valdivia <paulsantiagovaldivia@gmail.com>
2023-01-12 21:57:03 -0300

    refactor: siderbar

--- src/containers/App.svelte
+++ src/containers/App.svelte
@@ -1,6 +1,6 @@
 <script>
   import Header from "../components/Header.svelte";
-  import Silderbar from "../components/Silderbar.svelte";
+  import Silderbar from "../components/Siderbar.svelte";
   import Main from "../components/Main.svelte";
   import TimeLine from "../components/TimeLine.svelte";
 </script>
