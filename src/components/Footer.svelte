6e2cf6b60eea9d9f533e09f9e50ea59055dcf85c
Santiago Valdivia <paulsantiagovaldivia@gmail.com>
2023-01-12 22:14:27 -0300

    feat: footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,11 @@
+<script>
+  let year = new Date().getFullYear();
+</script>
+
+<div class="Footer">
+  <div class="Footer-container">
+    <div class="footer-copy">
+      © <span id="year">{year}</span>PUGSTAGRAM FROM PLATZI
+    </div>
+  </div>
+</div>
