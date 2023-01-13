b274c4232922326fd4cd07f8e3407fff107da02e
Santiago Valdivia <paulsantiagovaldivia@gmail.com>
2023-01-12 21:26:26 -0300

    create components and container

---
+++ src/containers/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Header from "../components/Header.svelte";
+  import Silderbar from "../components/Silderbar.svelte";
+  import Main from "../components/Main.svelte";
+  import TimeLine from "../components/TimeLine.svelte";
+</script>
+
+<style>
+  :global(body){
+    background-color: #676778;
+    color: white;
+  }
+</style>
+
+<Header />
+<Main>
+  <TimeLine />
+  <Silderbar />
+</Main>
